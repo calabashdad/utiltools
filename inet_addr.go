cd6e0419a0f6e5af1fe3c5fc7ed656c3672c8e88
yangkai <beyondyangkai@gmail.com>
2018-03-16 10:37:26 +0800

    add inet_addr

---
+++ inet_addr.go
@@ -0,0 +1,21 @@
+package utiltools
+
+import (
+	"strconv"
+	"strings"
+)
+
+// InetAddr transfer ip addr to uint32
+func InetAddr(ipaddr string) uint32 {
+	var (
+		ip                 = strings.Split(ipaddr, ".")
+		ip1, ip2, ip3, ip4 uint64
+		ret                uint32
+	)
+	ip1, _ = strconv.ParseUint(ip[0], 10, 8)
+	ip2, _ = strconv.ParseUint(ip[1], 10, 8)
+	ip3, _ = strconv.ParseUint(ip[2], 10, 8)
+	ip4, _ = strconv.ParseUint(ip[3], 10, 8)
+	ret = uint32(ip4)<<24 + uint32(ip3)<<16 + uint32(ip2)<<8 + uint32(ip1)
+	return ret
+}
